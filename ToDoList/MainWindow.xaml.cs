6eb6ba8d594c9505283daebfd5cea15410022cff
Felix Otte <felix.otte.2006@gmail.com>
2024-09-09 12:47:50 +0200

    Added functionality

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -16,9 +16,51 @@ namespace ToDoList
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Task> tasks = new List<Task>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void UpdateList()
+        {
+            lstTasks.Items.Clear();
+            foreach (Task task in tasks)
+            {
+                lstTasks.Items.Add(task.ToString());
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task(txtEingabe.Text);
+            tasks.Add(task);
+            txtEingabe.Text = "";
+            UpdateList();
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = lstTasks.SelectedIndex;
+
+            // wenn nichts ausgewählt wurde, dann wird index -1 sein, daher check
+            if (index >= 0)
+            {
+                tasks.RemoveAt(index);
+                UpdateList();
+            }
+        }
+
+        private void ButtonComplete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = lstTasks.SelectedIndex;
+
+            // wenn nichts ausgewählt wurde, dann wird index -1 sein, daher check
+            if (index >= 0)
+            {
+                tasks[index].CompleteTask();
+                UpdateList();
+            }
         }
     }
 }
---
+++ ToDoList/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class Task
+    {
+        public string Description { get; private set; }
+        public bool isComplete { get; private set; }
+
+        public DateTime date { get; private set; }
+
+        public Task(string description)
+        {
+            this.Description = description;
+            this.date = DateTime.Now;
+            isComplete = false;
+        }
+
+        public void CompleteTask()
+        {
+            isComplete = true;
+        }
+
+        public override string ToString()
+        {
+            //if (isComplete) is true say completed else say not completed
+            return $"{Description} / {(isComplete ? "Completed" : "Not Completed")} / {date.ToShortDateString()}";
+        }
+    }
+}
